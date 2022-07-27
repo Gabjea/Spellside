da72a97239485fc53c3dec4d731cbe390397a82e
Gabjea <gabjea856@gmail.com>
2022-07-27 21:19:32 +0300

    mint an nft

--- spellside/components/Dashboard.js
+++ spellside/components/Dashboard.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import DisplayNFTS from '../components/DisplayNFTS'
+import MintNft from '../components/MintNft'
 export default function Dashboard(props) {
    const user = props.user
   return (
     <div>
-       
+       <MintNft  />
     </div>
   )
 }
--- spellside/components/Navbar.js
+++ spellside/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar(props) {
             <div className="flex w-screen mb-5 py-6">
                 <h1 className="text-3xl ml-5">Spellside</h1>
                 <div className=" ml-auto mr-5 flex items-center">
-                    <h1 className="text-md mr-2 font-medium">{user.get("ethAddress")}</h1>
+                    <h1 className="text-md mr-2 font-mediumgit">{user.get("ethAddress")}</h1>
                     <button onClick={logout} className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center" >Log Out</button>
                 </div>
             </div>
